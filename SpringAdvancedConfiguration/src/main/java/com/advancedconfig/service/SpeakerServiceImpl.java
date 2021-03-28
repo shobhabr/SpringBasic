e0ecab615d6717495f2b8faa874902b43a1f90cf
shobhabr <shobhabr12@gmail.com>
2021-03-28 13:36:56 -0400

    Advanced Config

--- SpringAdvancedConfiguration/src/main/java/com/advancedconfig/service/SpeakerServiceImpl.java
+++ SpringAdvancedConfiguration/src/main/java/com/advancedconfig/service/SpeakerServiceImpl.java
@@ -4,6 +4,7 @@ import com.advancedconfig.model.Speaker;
 import com.advancedconfig.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("speakerService")
+@Profile("dev")//-Dspring.profiles.active=dev in vm
 public class SpeakerServiceImpl implements SpeakerService {
     @Autowired
     private SpeakerRepository repository;
