67d28ab151f954145ddad81c1598e39b1593c6e3
shobhabr <shobhabr12@gmail.com>
2021-03-27 22:43:59 -0400

    Xml Config and Setter injection

--- SpringBasic_XML/src/main/java/Application.java
+++ SpringBasic_XML/src/main/java/Application.java
@@ -1,20 +1,14 @@
 import com.xmlconfig.service.SpeakerService;
+import com.xmlconfig.service.SpeakerServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        //SpeakerService service= new SpeakerServiceImpl();
-        //System.out.println(service.findAll().get(0).getFirstName());
-        // use Spring Java Configuration
-        ApplicationContext applicationContext= new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext= new ClassPathXmlApplicationContext("applicationContext.xml");
         SpeakerService service= applicationContext.getBean("speakerService",SpeakerService.class);
-        System.out.println(service);
         System.out.println(service.findAll().get(0).getFirstName());
 
-        // check for single
-
-        SpeakerService service2= applicationContext.getBean("speakerService",SpeakerService.class);
-        System.out.println(service2);
     }
 }
--- SpringBasic_XML/src/main/java/com/xmlconfig/service/SpeakerServiceImpl.java
+++ SpringBasic_XML/src/main/java/com/xmlconfig/service/SpeakerServiceImpl.java
@@ -1,35 +1,22 @@
 package com.xmlconfig.service;
 
 import com.xmlconfig.model.Speaker;
+import com.xmlconfig.repository.HibernateSpeakerRepositoryImpl;
 import com.xmlconfig.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service("speakerService")
+
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository;
-
-    public SpeakerServiceImpl(){
-        System.out.println("SpeakerService No arg Constructor ");
-    }
-    @Autowired
-    public SpeakerServiceImpl(SpeakerRepository speakerRepository){
-        System.out.println("SpeakerRepository Constructor ");
-        repository=speakerRepository;
-    }
-
-
+    private SpeakerRepository repository ;
     public List<Speaker>  findAll(){
         return repository.findAll();
     }
 
-    // for setter injection
-
-    public void setRepository(SpeakerRepository repository) {
-        System.out.println("SpeakerService Setter ");
+    public void setSpeakerRepository(SpeakerRepository repository) {
         this.repository = repository;
     }
 }
